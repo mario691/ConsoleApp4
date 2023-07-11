58ce281c8a5819cffb3a7bba5c026d8aa01cb98f
mario691 <121613035+mario691@users.noreply.github.com>
2023-07-11 13:47:42 -0700

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            // Shkruani nje program qe printon n numrat e pare te plote tek
+            //Console.Write("Shkruani numrin e pare te plote tek (n):");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Numrat e pare te plote tek deri ne {n} jane: ");
+
+            //for(int i = 1; i <= n; i+=2)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.ReadLine();
+            //}
+
+
+
+
+            //Shkruani nje program qe gjen siperfaqen e nje drejtkendeshi ku gjatesia dhe gjeresia jepen nga perdoruesi
+
+
+            //Console.Write("Shkruani gjatesine e drejtkendeshit:");
+            //double gjatesia = Convert.ToDouble(Console.ReadLine());    
+
+            //Console.Write("Shkruani gjeresine e drejtkendeshit:");
+            //double gjeresia = Convert.ToDouble(Console.ReadLine()); 
+
+
+
+            //double siperfaqja = gjatesia * gjeresia;
+
+            //Console.WriteLine($"Siperfaqja e drejtkëndëshit është: {siperfaqja}");
+            //Console.ReadLine();
+
+
+
+            // Shkruani një program që llogarit perimetrin dhe siperfaqen e një rrethi duke përdorur rrezen e dhënë nga përdoruesi
+            //Console.Write("Jepni rrezen e rrethit:");
+            //double rrezja = Convert.ToDouble(Console.ReadLine());
+
+            //double perimetri = 2 * Math.PI * rrezja;
+            //double siperfaqja = Math.PI * Math.Pow(rrezja, 2);
+
+            //Console.WriteLine($"Perimetri i rrethit eshte : {perimetri}");
+            //Console.WriteLine($"Siperfaqja e rrethit eshte: {siperfaqja}");
+            //Console.ReadLine();
+
+
+
+            // Shkruani një program që merr dy numra të plotë si input dhe printon shumën, diferencën, produktin dhe mbetjen nga pjestimi i me te madhit prej tyre.
+
+            //Console.Write("Shkruani numrin e parë: ");
+            //int numri1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Shkruani numrin e dytë: ");
+            //int numri2 = Convert.ToInt32(Console.ReadLine());
+
+            //int shuma = numri1 + numri2;
+            //int diference = numri1 - numri2;
+            //int produkti = numri1 * numri2;
+            //int mbetja = numri1 % numri2;
+
+            //Console.WriteLine($"Shuma: {shuma}");
+            //Console.WriteLine($"Diferenca: {diference}");
+            //Console.WriteLine($"Produkti: {produkti}");
+            //Console.WriteLine($"Mbetja: {mbetja}");
+            //Console.ReadLine();
+
+
+
+            //Shkruani një program që njehson shumën e 10 numrave të parë natyrorë.
+            //int n = 10; 
+            //int shuma = 0;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    shuma += i;
+            //}
+
+            //Console.WriteLine($"Shuma e {n} numrave të parë natyrorë është: {shuma}");
+            //Console.ReadLine();
+
+
+
+            //Shkruani një program që përcakton nëse 2 numra te dhene x, y jane pika te drejtezes y = 3x ^ 2 + 5x - 6.
+            //Console.Write("Shkruani vlerën e x: ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Shkruani vlerën e y: ");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //double vlera_y = 3 * Math.Pow(x, 2) + 5 * x - 6;
+
+            //if (Math.Abs(y - vlera_y) < 0.0001)
+            //{
+            //    Console.WriteLine($"Pikat ({x}, {y}) janë në drejten y = 3x^2 + 5x - 6.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Pikat ({x}, {y}) nuk janë në drejten y = 3x^2 + 5x - 6.");
+
+            //}
+            //Console.ReadLine();
+
+
+            //Shkruani një program që i kërkon përdoruesit të fusë një temperaturë në Fahrenheit dhe e konverton atë në Celsius.
+
+            //Console.Write("Jepni temperaturen ne Fahrenheit:");
+            //double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            //double celcius = (fahrenheit - 32) * 5 / 9;
+
+            //Console.WriteLine("Temperatura ne celcius eshte:" + celcius);
+            //Console.ReadLine();
+
+
+            //Shkruani një program që i kërkon përdoruesit të fusë një fjali dhe më pas afishon numrin e fjalëve në fjali, fjalet duhet te kene gjatesi te pakten 3 karaktere qe te quhen te tilla.
+            //            Console.Write("Shkruani nje fjali:");
+            //            string fjalia = Console.ReadLine();
+
+            //            string[] fjalet = fjalia.Split(' ');
+            //            int numriFjaleve = 0;
+
+            //            foreach (string fjala in fjalet)
+            //            {
+            //                if (fjala.Length >= 3)
+            //                {
+            //                    numriFjaleve++;
+            //                    }
+                         // }
+//            Console.WriteLine(numriFjaleve);
+//            Console.ReadLine();
+
+
+
+        }
+   }
+}
